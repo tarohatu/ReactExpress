7a313de05d23d18b531cfbbb6ac267a1142340d9
tarohatu <tarohatu2@gmail.com>
2019-07-31 20:55:28 +0900

    express+webpack+babel

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+app.use(express.static(path.join('./', 'dist')));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.listen(3000, ()=> {
+  console.log('server running');
+});
