daa4ef50373d1307d5961e72c7bcba0fe5af5f5c
tarohatu <tarohatu2@gmail.com>
2019-08-03 22:57:44 +0900

    edit location model

--- models/location.js
+++ models/location.js
@@ -1,4 +1,14 @@
 'use strict';
+
+const modelOption = (models, as) => {
+  var seqOption = {
+      model: models.user,
+      as: as,
+      attributes: ['id', 'user_name', 'name']
+  };
+  return seqOption
+}
+
 module.exports = (sequelize, DataTypes) => {
   const location = sequelize.define('location', {
     longitude: DataTypes.DOUBLE,
@@ -6,13 +16,28 @@ module.exports = (sequelize, DataTypes) => {
     user_id: DataTypes.INTEGER
   }, {
     underscored: true,
-  });
+  })
+
   location.associate = function(models) {
     location.belongsTo(models.user, {
       foreignKey: 'user_id',
       targetKey: 'id',
       as: 'user'
     })
-  };
+  }
+
+  location.allLocations = async (models) => {
+    let option = {
+      include: [
+          modelOption(models, 'user'),
+      ]
+    }
+    let locations = await location.findAll(option)
+    .catch(error => {
+      throw error
+    })
+    return locations
+  }
+
   return location;
-};
+}
--- routes/location.js
+++ routes/location.js
@@ -3,28 +3,28 @@ var db = require('../models');
 var router = express.Router();
 
 router.get('/', async (req, res, next) => { 
-  let locations = await db.location.findAll()
+  let locations = await db.location.allLocations(db)
   .catch(error => {
-    res.send(error)
+    return res.send(error)
   })
   return res.json(locations)
 })
 
 router.post('/', async (req, res, next) => {
-    try {
-        let user = await db.user.findById(req.body.user_id)
-        if(!user) {
-            return res.sendStatus(404)
-        }
-        let result = await db.location.create({
-            user_id: user.id,
-            latitude: req.body.latitude,
-            longitude: req.body.longitude
-        })
-        return res.json(result)
-    } catch (error) {
-        return res.send(error)
+  try {
+    let user = await db.user.findById(req.body.user_id)
+    if(!user) {
+      return res.sendStatus(404)
     }
+    let result = await db.location.create({
+      user_id: user.id,
+      latitude: req.body.latitude,
+      longitude: req.body.longitude
+    })
+    return res.json(result)
+  } catch (error) {
+    return res.send(error)
+  }
 })
 
 module.exports = router
